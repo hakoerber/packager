8ca117c9f2fe67a63ef6d736939bb8c0841868b7
Hannes Körber <hannes.koerber@haktec.de>
2017-11-15 20:08:11 +0100

    backend poc done

--- backend/packager-server/main.py
+++ backend/packager-server/main.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+def get_lists():
+    return [
+        "list_item_1",
+        "list_item_2",
+        "list_item_3",
+    ]
 
 @app.route('/')
 def index():
@@ -14,6 +21,10 @@ def api():
 @app.route('/api/v1/')
 def apiv1():
     return "Hello, API version 1!"
+
+@app.route('/api/v1/lists/')
+def apiv1_lists():
+    return jsonify(get_lists())
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
