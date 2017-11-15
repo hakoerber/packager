1df231b49fb48a497ae8bc5bcabeb9902e55129f
Hannes Körber <hannes.koerber@haktec.de>
2017-11-15 21:12:24 +0100

    tmp

--- frontend/src/elm/Main.elm
+++ frontend/src/elm/Main.elm
@@ -1,63 +1,76 @@
 module Main exposing (..)
+
 import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing ( onClick )
-
--- component import example
-import Components.Hello exposing ( hello )
+import Html.Events exposing (..)
+import Random exposing (..)
 
 
--- APP
+-- App
+
+
 main : Program Never Model Msg
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
--- MODEL
-type alias Model = Int
 
-model : Model
-model = 0
+-- Model
 
 
--- UPDATE
-type Msg = NoOp | Increment
+type alias Model =
+    { lists : List Int }
 
-update : Msg -> Model -> Model
+
+init : ( Model, Cmd Msg )
+init =
+    ( Model [], Cmd.none )
+
+
+
+-- Update
+
+
+type Msg
+    = LoadLists
+    | GetLists (List Int)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    NoOp -> model
-    Increment -> model + 1
+    case msg of
+        LoadLists ->
+            ( model
+            , Random.generate GetLists (Random.list 6 (int 0 100))
+            )
+
+        GetLists lists ->
+            ( model
+            , Cmd.none
+            )
 
 
--- VIEW
--- Html is defined as: elem [ attribs ][ children ]
--- CSS can be applied via class names or inline style attrib
+
+-- Subscriptions
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- View
+
+
 view : Model -> Html Msg
 view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          img [ src "static/img/elm.jpg", style styles.img ] []                             -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
-          ]
+    div []
+        [ h1 [] [ text "lists" ]
+        , button [ onClick LoadLists ] [ text "load lists" ]
+        , div [] [ text (toString model.lists) ]
         ]
-      ]
-    ]
-  ]
-
-
--- CSS STYLES
-styles : { img : List ( String, String ) }
-styles =
-  {
-    img =
-      [ ( "width", "33%" )
-      , ( "border", "4px solid #337AB7")
-      ]
-  }
