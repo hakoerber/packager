d1a444f67d74ac9cadbcf3e5752ff0112672b439
Hannes Körber <hannes@hkoerber.de>
2021-09-07 18:48:34 +0200

    WIP

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,9 +1,9 @@
 use std::convert::Infallible;
 
+use serde::Serialize;
+use uuid::Uuid;
 use warp::http::StatusCode;
 use warp::Filter;
-use uuid::Uuid;
-use serde::Serialize;
 
 use packager;
 
@@ -66,35 +66,34 @@ impl<'a> Trip<'a> {
     }
 }
 
+// for list in &lists {
+//     println!("Contents of package list {:?}:", list.name);
+//     for item in &list.items {
+//         println!("\t{:?}", item);
+//     }
+// }
 
-    // for list in &lists {
-    //     println!("Contents of package list {:?}:", list.name);
-    //     for item in &list.items {
-    //         println!("\t{:?}", item);
-    //     }
-    // }
+// println!("\nNow we're starting an actual trip!");
 
-    // println!("\nNow we're starting an actual trip!");
+// let mut trip = Trip::from_package_list(
+//     String::from("Campingtrip"),
+//     String::from("2021-09-06"),
+//     &lists[0],
+// );
 
-    // let mut trip = Trip::from_package_list(
-    //     String::from("Campingtrip"),
-    //     String::from("2021-09-06"),
-    //     &lists[0],
-    // );
+// println!(
+//     "\nPackage list for trip {:?} at {:?}:",
+//     trip.name, trip.date
+// );
+// for item in &trip.list.items {
+//     println!("{:?}", item);
+// }
 
-    // println!(
-    //     "\nPackage list for trip {:?} at {:?}:",
-    //     trip.name, trip.date
-    // );
-    // for item in &trip.list.items {
-    //     println!("{:?}", item);
-    // }
-
-    // trip.list.items[0].set_status(TripItemStatus::Ready);
-    // trip.list.items[1].set_status(TripItemStatus::Packed);
-    // for item in &trip.list.items {
-    //     println!("{:?}", item);
-    // }
+// trip.list.items[0].set_status(TripItemStatus::Ready);
+// trip.list.items[1].set_status(TripItemStatus::Packed);
+// for item in &trip.list.items {
+//     println!("{:?}", item);
+// }
 
 #[derive(Serialize)]
 struct ErrorMessage {
