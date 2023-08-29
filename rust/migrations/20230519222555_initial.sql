040504da65ba396f95ae2d72a624aed7fcd0b90a
Hannes Körber <hannes@hkoerber.de>
2023-08-29 21:33:59 +0200

    merge migrations

--- rust/migrations/20230519222555_initial.sql
+++ rust/migrations/20230519222555_initial.sql
@@ -10,48 +10,49 @@ CREATE TABLE "inventory_items" (
 CREATE UNIQUE INDEX ux_unique ON inventory_items(name, category_id);
 
 CREATE TABLE "inventory_items_categories" (
-	id VARCHAR(36) NOT NULL,
-	name TEXT NOT NULL,
-	description TEXT,
-	PRIMARY KEY (id),
-	UNIQUE (name)
+    id VARCHAR(36) NOT NULL,
+    name TEXT NOT NULL,
+    description TEXT,
+    PRIMARY KEY (id),
+    UNIQUE (name)
 );
 
 CREATE TABLE "trips" (
-	id VARCHAR(36) NOT NULL,
-	name TEXT NOT NULL,
-	date_start DATE NOT NULL,
-	date_end DATE NOT NULL,
+    id VARCHAR(36) NOT NULL,
+    name TEXT NOT NULL,
+    date_start DATE NOT NULL,
+    date_end DATE NOT NULL,
     location TEXT,
     state VARCHAR(8) NOT NULL,
     comment TEXT,
     temp_min INTEGER,
     temp_max INTEGER,
-	PRIMARY KEY (id),
-	UNIQUE (name)
+    PRIMARY KEY (id),
+    UNIQUE (name)
 );
 
 CREATE TABLE "trips_types" (
-	id VARCHAR(36) NOT NULL,
-	name TEXT NOT NULL,
-	PRIMARY KEY (id),
-	UNIQUE (name)
+    id VARCHAR(36) NOT NULL,
+    name TEXT NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (name)
 );
 
 CREATE TABLE "trips_to_trips_types" (
-	trip_id VARCHAR(36) NOT NULL,
-	trip_type_id VARCHAR(36) NOT NULL,
-	PRIMARY KEY (trip_id, trip_type_id),
-	FOREIGN KEY(trip_id) REFERENCES "trips" (id),
-	FOREIGN KEY(trip_type_id) REFERENCES "trips_types" (id)
+    trip_id VARCHAR(36) NOT NULL,
+    trip_type_id VARCHAR(36) NOT NULL,
+    PRIMARY KEY (trip_id, trip_type_id),
+    FOREIGN KEY(trip_id) REFERENCES "trips" (id),
+    FOREIGN KEY(trip_type_id) REFERENCES "trips_types" (id)
 );
 
 CREATE TABLE trips_items (
-	item_id VARCHAR(36) NOT NULL,
-	trip_id VARCHAR(36) NOT NULL,
-	pick BOOLEAN NOT NULL,
-	pack BOOLEAN NOT NULL,
-	PRIMARY KEY (item_id, trip_id),
-	FOREIGN KEY(item_id) REFERENCES "inventory_items" (id),
-	FOREIGN KEY(trip_id) REFERENCES "trips" (id)
+    item_id VARCHAR(36) NOT NULL,
+    trip_id VARCHAR(36) NOT NULL,
+    pick BOOLEAN NOT NULL,
+    pack BOOLEAN NOT NULL,
+    new BOOLEAN NOT NULL,
+    PRIMARY KEY (item_id, trip_id),
+    FOREIGN KEY(item_id) REFERENCES "inventory_items" (id),
+    FOREIGN KEY(trip_id) REFERENCES "trips" (id)
 );
--- rust/migrations/20230520131340_1.sql
+++
@@ -1,2 +0,0 @@
--- Add migration script here
-ALTER TABLE "trips_items" ADD COLUMN new BOOLEAN NOT NULL;
