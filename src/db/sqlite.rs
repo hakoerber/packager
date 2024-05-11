ea4945307753fec32b2f4534537491cd4035d5eb
Hannes Körber <hannes@hkoerber.de>
2024-05-11 13:54:50 +0200

    tmp

--- src/db/sqlite.rs
+++ src/db/sqlite.rs
@@ -4,11 +4,11 @@ use tracing::Instrument;
 
 use sqlx::sqlite::{SqliteConnectOptions, SqlitePoolOptions};
 use sqlx::ConnectOptions;
-pub use sqlx::{Pool as SqlitePool, Sqlite};
+// pub use sqlx::{Pool as SqlitePool, Sqlite};
 
 use std::str::FromStr as _;
 
-pub use sqlx::Type;
+// pub use sqlx::Type;
 
 use crate::StartError;
 
@@ -20,40 +20,49 @@ pub fn int_to_bool(value: i32) -> bool {
     }
 }
 
-#[tracing::instrument]
-pub async fn init_database_pool(url: &str) -> Result<sqlx::Pool<sqlx::Sqlite>, StartError> {
-    async {
-        SqlitePoolOptions::new()
-            .max_connections(5)
-            .connect_with(
-                SqliteConnectOptions::from_str(url)?
-                    .log_statements(log::LevelFilter::Debug)
-                    .log_slow_statements(log::LevelFilter::Warn, time::Duration::from_millis(100))
-                    .pragma("foreign_keys", "1"),
-            )
-            .await
+pub struct DB;
+
+impl super::Database for DB {
+    type Pool = sqlx::Pool<sqlx::Sqlite>;
+
+    #[tracing::instrument]
+    async fn init_database_pool(url: &str) -> Result<Self::Pool, StartError> {
+        async {
+            SqlitePoolOptions::new()
+                .max_connections(5)
+                .connect_with(
+                    SqliteConnectOptions::from_str(url)?
+                        .log_statements(log::LevelFilter::Debug)
+                        .log_slow_statements(
+                            log::LevelFilter::Warn,
+                            time::Duration::from_millis(100),
+                        )
+                        .pragma("foreign_keys", "1"),
+                )
+                .await
+        }
+        .instrument(tracing::info_span!("packager::sql::pool"))
+        .await
+        .map_err(Into::into)
     }
-    .instrument(tracing::info_span!("packager::sql::pool"))
-    .await
-    .map_err(Into::into)
-}
 
-#[tracing::instrument]
-pub async fn migrate(url: &str) -> Result<(), StartError> {
-    async {
-        let pool = SqlitePoolOptions::new()
-            .max_connections(5)
-            .connect_with(
-                SqliteConnectOptions::from_str(url)?
-                    .pragma("foreign_keys", "0")
-                    .log_statements(log::LevelFilter::Debug),
-            )
-            .await?;
+    #[tracing::instrument]
+    async fn migrate(url: &str) -> Result<(), StartError> {
+        async {
+            let pool = SqlitePoolOptions::new()
+                .max_connections(5)
+                .connect_with(
+                    SqliteConnectOptions::from_str(url)?
+                        .pragma("foreign_keys", "0")
+                        .log_statements(log::LevelFilter::Debug),
+                )
+                .await?;
 
-        sqlx::migrate!().run(&pool).await
+            sqlx::migrate!().run(&pool).await
+        }
+        .instrument(tracing::info_span!("packager::sql::migrate"))
+        .await?;
+
+        Ok(())
     }
-    .instrument(tracing::info_span!("packager::sql::migrate"))
-    .await?;
-
-    Ok(())
 }
